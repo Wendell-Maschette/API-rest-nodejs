4fcd56c5f42039f4f8bac5e7dae8d13e319cf7f3
Wendell Maschette <wendell.maschette@gmail.com>
2021-11-05 15:55:59 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+require('./controllers/authController')(app);
+
+app.listen(3000);
